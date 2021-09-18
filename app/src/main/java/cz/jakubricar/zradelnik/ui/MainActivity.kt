235660ddd66088163c1a82ae62eba19a7dd30d09
Jakub Řičař <jakub.ricar@gmail.com>
2021-09-18 22:54:39 +0200

    Test insets

--- app/src/main/java/cz/jakubricar/zradelnik/ui/MainActivity.kt
+++ app/src/main/java/cz/jakubricar/zradelnik/ui/MainActivity.kt
@@ -3,12 +3,23 @@ package cz.jakubricar.zradelnik.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsPadding
+import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import cz.jakubricar.zradelnik.ui.theme.ZradelnikTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +30,33 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ZradelnikTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                ProvideWindowInsets {
+                    val systemUiController = rememberSystemUiController()
+                    val darkIcons = MaterialTheme.colors.isLight
+
+                    SideEffect {
+                        systemUiController.setSystemBarsColor(
+                            Color.Transparent,
+                            darkIcons = darkIcons
+                        )
+                    }
+
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        color = MaterialTheme.colors.background,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .verticalScroll(rememberScrollState())
+                                .statusBarsPadding()
+                                .navigationBarsPadding()
+                        ) {
+                            for (i in 1..100) {
+                                Greeting("Android $i")
+                            }
+                        }
+                    }
                 }
             }
         }
