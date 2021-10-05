513bce549a28fc4dbb0a460cae6778f75aa0744b
Jakub Řičař <jakub.ricar@gmail.com>
2021-10-05 17:01:33 +0200

    Change theme when theme settings change

--- app/src/main/java/cz/jakubricar/zradelnik/ui/ZradelnikApp.kt
+++ app/src/main/java/cz/jakubricar/zradelnik/ui/ZradelnikApp.kt
@@ -1,16 +1,47 @@
 package cz.jakubricar.zradelnik.ui
 
+import android.content.SharedPreferences
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.preference.PreferenceManager
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import cz.jakubricar.zradelnik.getTheme
+import cz.jakubricar.zradelnik.model.Settings
 import cz.jakubricar.zradelnik.ui.theme.ZradelnikTheme
 
 @Composable
 fun ZradelnikApp() {
-    ZradelnikTheme {
+    val context = LocalContext.current
+    var theme by remember {
+        mutableStateOf(PreferenceManager.getDefaultSharedPreferences(context).getTheme())
+    }
+
+    DisposableEffect(context) {
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
+            if (key == Settings.Keys.THEME) {
+                theme = prefs.getTheme()
+            }
+        }
+
+        val prefManager = PreferenceManager.getDefaultSharedPreferences(context).apply {
+            registerOnSharedPreferenceChangeListener(listener)
+        }
+
+        onDispose {
+            prefManager.unregisterOnSharedPreferenceChangeListener(listener)
+        }
+    }
+
+    ZradelnikTheme(theme = theme) {
         ProvideWindowInsets {
             val systemUiController = rememberSystemUiController()
             val darkIcons = MaterialTheme.colors.isLight
--- app/src/main/java/cz/jakubricar/zradelnik/ui/theme/Theme.kt
+++ app/src/main/java/cz/jakubricar/zradelnik/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import cz.jakubricar.zradelnik.model.Settings
 
 private val LightColorPalette = lightColors(
     primary = Indigo500,
@@ -23,7 +24,16 @@ private val DarkColorPalette = darkColors(
 )
 
 @Composable
-fun ZradelnikTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ZradelnikTheme(
+    theme: Settings.Theme = Settings.Theme.DEFAULT,
+    content: @Composable () -> Unit
+) {
+    val darkTheme = when (theme) {
+        Settings.Theme.LIGHT -> false
+        Settings.Theme.DARK -> true
+        Settings.Theme.DEFAULT -> isSystemInDarkTheme()
+    }
+
     MaterialTheme(
         colors = if (darkTheme) DarkColorPalette else LightColorPalette,
         typography = Typography,
