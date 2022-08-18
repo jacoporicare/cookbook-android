fdcda8999a770b5f6bb3f796b44313d833ef07b1
Jakub Řičař <jakub.ricar@gmail.com>
2022-08-18 22:15:44 +0200

    Upgrade AGP and deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath(kotlin("gradle-plugin", "1.6.10"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.gms:google-services:4.3.13")
     }
 }
 
