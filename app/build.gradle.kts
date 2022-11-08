c1cf1f58a5650c17900405a197913308241ce998
Jakub Řičař <jakub.ricar@gmail.com>
2022-11-08 19:16:05 +0100

    Upgrade deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,11 +116,11 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging-ktx")
 
     // Jetpack Compose
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.compose.material3:material3:1.0.0-beta03")
-    implementation("androidx.compose.material:material-icons-extended:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
+    implementation("androidx.compose.ui:ui:1.3.0")
+    implementation("androidx.compose.material:material:1.3.0")
+    implementation("androidx.compose.material3:material3:1.0.0")
+    implementation("androidx.compose.material:material-icons-extended:1.3.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0")
 
     // Accompanist
     implementation("com.google.accompanist:accompanist-insets:0.19.0")
@@ -130,14 +130,14 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.5.2")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 
     // Work
     implementation("androidx.work:work-runtime-ktx:2.7.1")
@@ -167,10 +167,10 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.2.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.3.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0")
 }
 
 apollo {
