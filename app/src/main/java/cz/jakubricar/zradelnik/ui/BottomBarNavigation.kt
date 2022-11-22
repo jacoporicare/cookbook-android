95f662954a11b98737d4cfc8793e16aa8e894c4a
Jakub Řičař <jakub.ricar@gmail.com>
2022-11-22 20:00:20 +0100

    Replace old BottomNavigation with Material 3 NavigationBar

--- app/src/main/java/cz/jakubricar/zradelnik/ui/BottomBarNavigation.kt
+++ app/src/main/java/cz/jakubricar/zradelnik/ui/BottomBarNavigation.kt
@@ -3,33 +3,29 @@ package cz.jakubricar.zradelnik.ui
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MenuBook
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.google.accompanist.insets.LocalWindowInsets
-import com.google.accompanist.insets.rememberInsetsPaddingValues
-import com.google.accompanist.insets.ui.BottomNavigation
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.insets.ui.Scaffold
 import cz.jakubricar.zradelnik.R
+import cz.jakubricar.zradelnik.ui.theme.ZradelnikTheme
 
 sealed class Screen(
     val route: String,
@@ -68,35 +64,16 @@ fun BottomBarNavigation(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    BottomNavigation(
-        contentPadding = rememberInsetsPaddingValues(
-            insets = LocalWindowInsets.current.navigationBars,
-        ),
-        backgroundColor = MaterialTheme.colors.surface,
-    ) {
+    NavigationBar {
         screens.forEach { screen ->
             val selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
 
-            val tint = if (selected) {
-                MaterialTheme.colors.primary
-            } else {
-                LocalContentColor.current
-            }
-
-            val contentAlpha = if (selected) {
-                ContentAlpha.high
-            } else {
-                ContentAlpha.medium
-            }
-
-            BottomNavigationItem(
+            NavigationBarItem(
                 icon = {
                     screen.imageVector?.let { imageVector ->
                         Icon(
                             imageVector = imageVector,
                             contentDescription = stringResource(screen.labelId),
-                            modifier = Modifier.alpha(contentAlpha),
-                            tint = tint
                         )
                     }
 
@@ -104,13 +81,11 @@ fun BottomBarNavigation(navController: NavController) {
                         Icon(
                             painter = painterResource(iconId),
                             contentDescription = stringResource(screen.labelId),
-                            modifier = Modifier
-                                .size(24.dp)
-                                .alpha(contentAlpha),
-                            tint = tint,
+                            modifier = Modifier.size(24.dp),
                         )
                     }
                 },
+                label = { Text(text = stringResource(screen.labelId)) },
                 selected = selected,
                 onClick = {
                     navController.navigate(screen.route) {
@@ -126,17 +101,22 @@ fun BottomBarNavigation(navController: NavController) {
                         // Restore state when reselecting a previously selected item
                         restoreState = true
                     }
-                },
-                label = {
-                    CompositionLocalProvider(LocalContentAlpha provides contentAlpha) {
-                        Text(
-                            text = stringResource(screen.labelId),
-                            modifier = Modifier.alpha(contentAlpha),
-                            color = tint,
-                        )
-                    }
                 }
             )
         }
+    }
+}
+
+
+@Preview(heightDp = 200)
+@Composable
+fun DefaultPreview() {
+    ZradelnikTheme {
+        Scaffold(
+            bottomBar = { BottomBarNavigation(navController = rememberNavController()) }
+        ) {
+            Text(text = "Bottom Bar Navigation")
+        }
+
     }
 }
